eb7d79ce5fadeba62d98f536a43b144bcc7db113
Andy Lai <lai.andyk@gmail.com>
2018-05-03 07:53:54 -0500

    Both autograders passed

--- mp8.py
+++ mp8.py
@@ -5,6 +5,37 @@ import skimage
 import csv
 from matplotlib import pyplot as plt
 
+def calcEnergy(Q):
+	Eq = np.zeros(20)
+	for im in np.arange(20):
+		# EX[logQ]
+		Qim = np.squeeze( Q[im,:,:] )
+		ElogQ = np.sum( np.sum( ( Qim * np.log( Qim + eps ) ) + ( (1-Qim) * np.log( (1-Qim) + eps ) ) ) )
+		# EX[logP] (a little more involved)
+		Eqk = 2 * Qim - 1 # also a 28 x 28 array
+		#hSum = ( np.sum( np.sum( Eqk[0:26,:] ) * Eqk[1:27,:] ) + np.sum( np.sum( Eqk[:,0:26] * Eqk[:,1:27] ) ) ) * 2 * thetaHH
+		hSum = 0
+		for r in np.arange(28):
+			for c in np.arange(28):
+				if r - 1 > -1:
+					hSum += thetaHH * Eqk[r,c] * Eqk[r-1,c]
+				if r + 1 < 28:
+					hSum += thetaHH * Eqk[r,c] * Eqk[r+1,c]
+				if c - 1 > -1:
+					hSum += thetaHH * Eqk[r,c] * Eqk[r,c-1]
+				if c + 1 < 28:
+					hSum += thetaHH * Eqk[r,c] * Eqk[r,c+1]
+		xSum = np.sum( np.sum( Eqk * noiseImages[im,:,:] ) ) * thetaHX
+		ElogP = hSum + xSum
+		#if im == 1:
+			#print("Normal hSum: ", hSum)
+			#print(np.sum( np.sum( Eqk[ 0:26,:] ) * Eqk[1:27,:] ))
+			#print(np.sum( np.sum( Eqk[:,0:26] * Eqk[:,1:27] ) ))
+			#print(Eqk[0:26,:] * Eqk[1:27,:])	
+			#print("Normal ElogP: ", ElogP)
+		Eq[im] = ElogQ - ElogP
+
+	return Eq
 
 images = mnist.train_images()
 images = images[:20,:,:]
@@ -33,12 +64,6 @@ for i in np.arange(20):
 	#print(noiseCoordinates[i,:,1])
 	noiseImages[i,noiseCoordinates[i,:,0],noiseCoordinates[i,:,1]] = -1 * images[i,noiseCoordinates[i,:,0],noiseCoordinates[i,:,1]]
 
-# check for correctness
-#print(np.count_nonzero(np.not_equal(images, noiseImages)))
-
-#plt.imshow(noiseImages[0,:,:], interpolation = 'nearest')
-#plt.show()
-
 # Read in Initial Parameters (same for every image)
 Q_init = np.zeros( (1, 28, 28) )
 i = 0
@@ -64,23 +89,21 @@ thetaHH = 0.8
 thetaHX = 2
 eps = 10**(-10)
 
-# Initial energy terms
-Eq_init = np.zeros( (1, 20) )
-H = np.ones( (1, 28, 28) )
-H[Q_init < .5] = -1
+# Initial energy term
+Eq = np.zeros( (20, 11) )
+Q = np.ones( (20, 28, 28) ) * Q_init
+Eq[:,0] = calcEnergy(Q)
 
 
 # Start MFI and calculate energy terms
 #Q_new = np.ones( (20, 28, 28) ) * Q_init
-Q = np.ones( (20, 28, 28) ) * Q_init
-#test_denom = np.zeros(28)
-for iter in np.arange(10):
+for itr in np.arange(10):
 	#Q_old = np.copy(Q_new)
 	for im in np.arange(20):
 		for pxl in np.arange(28*28):
 			r = pixelOrder[im,pxl,0] # update row pixel
 			c = pixelOrder[im,pxl,1] # update col pixel
-			X = noiseImages[im,r,c]
+			Xj = noiseImages[im,r,c]
 			hSum = 0
 			if r - 1 > -1:
 				hSum += thetaHH * (2 * Q[im, r-1, c] - 1)
@@ -90,17 +113,30 @@ for iter in np.arange(10):
 				hSum += thetaHH * (2 * Q[im, r, c-1] - 1)
 			if c + 1 < 28:
 				hSum += thetaHH * (2 * Q[im, r, c+1] - 1)
-			xSum = thetaHX * X
+			xSum = thetaHX * Xj
 			logqiPos = hSum + xSum
 			Q[im, r, c]  = np.exp(logqiPos) / (np.exp(logqiPos) + np.exp(-logqiPos))
-			#print((np.exp(logqiPos) + np.exp(-logqiPos)))
 
+	Eq[:,itr+1] = calcEnergy(Q)
 
-
-#print(Q[1,:,:])
 cleanImages = np.ones( (20, 28, 28) )
 cleanImages[Q < 0.5] = 0
+#for i, img in enumerate(cleanImages):
+#	plt.imsave('img' + str(i) + '.jpg', img)
 
-plt.imshow(cleanImages[1,:,:], interpolation='nearest')
+submitImages = np.zeros((28,280))
+otherImages = np.zeros((28,280))
+for i in np.arange(10):
+	submitImages[:,(28*i)+np.arange(28)] = cleanImages[10+i,:,:]
+	otherImages[:,(28*i)+np.arange(28)] = cleanImages[i,:,:]
+
+np.savetxt('denoised.csv', submitImages, delimiter = ',', fmt = '%d')
+np.savetxt('energy.csv', Eq[10:12,0:2], delimiter = ',', fmt = '%f')
+
+
+#plt.imshow(otherImages)
+#plt.show()
+
+#plt.imshow(cleanImages[1,:,:], interpolation='nearest')
 #plt.imshow(noiseImages[0,:,:], interpolation='nearest')
-plt.show()
+#plt.show()
